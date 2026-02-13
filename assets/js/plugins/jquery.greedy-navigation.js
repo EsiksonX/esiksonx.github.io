c2a6277f42312551061ba7f45ca510a115d5bf30
Yang Xiang <yang-xiang01@outlook.com>
2026-02-13 17:22:57 +0800

    fix navigate bar display bugs on mobile devices

--- assets/js/plugins/jquery.greedy-navigation.js
+++ assets/js/plugins/jquery.greedy-navigation.js
@@ -8,65 +8,68 @@
 var $nav = $('#site-nav');
 var $btn = $('#site-nav button');
 var $vlinks = $('#site-nav .visible-links');
-var $vlinks_persist_tail = $vlinks.children("*.persist.tail");
+var $vlinks_persist_tail = $vlinks.children('*.persist.tail');
 var $hlinks = $('#site-nav .hidden-links');
 
 var breaks = [];
+var mobileBreakpoint = 924;
+
+function updateLayoutOffsets() {
+  var mastheadHeight = $('.masthead').height();
+  $('body').css('padding-top', mastheadHeight + 'px');
+  if ($('.author__urls-wrapper button').is(':visible')) {
+    $('.sidebar').css('padding-top', '');
+  } else {
+    $('.sidebar').css('padding-top', mastheadHeight + 'px');
+  }
+}
+
+function moveHiddenToVisible() {
+  while ($hlinks.children().length > 0) {
+    if ($vlinks_persist_tail.length > 0) {
+      $hlinks.children().first().insertBefore($vlinks_persist_tail);
+    } else {
+      $hlinks.children().first().appendTo($vlinks);
+    }
+  }
+}
 
 function updateNav() {
+  var isMobile = window.matchMedia && window.matchMedia('(max-width: ' + mobileBreakpoint + 'px)').matches;
+
+  if (isMobile) {
+    while ($vlinks.children('*:not(.persist)').length > 0) {
+      $vlinks.children('*:not(.persist)').last().prependTo($hlinks);
+    }
+
+    $btn.removeClass('hidden');
+    $btn.attr('count', $hlinks.children().length);
+    updateLayoutOffsets();
+    return;
+  }
+
+  moveHiddenToVisible();
+  breaks = [];
 
   var availableSpace = $btn.hasClass('hidden') ? $nav.width() : $nav.width() - $btn.width() - 30;
 
-  // The visible list is overflowing the nav
   if ($vlinks.width() > availableSpace) {
-
-    while ($vlinks.width() > availableSpace && $vlinks.children("*:not(.persist)").length > 0) {
-      // Record the width of the list
+    while ($vlinks.width() > availableSpace && $vlinks.children('*:not(.persist)').length > 0) {
       breaks.push($vlinks.width());
-
-      // Move item to the hidden list
-      $vlinks.children("*:not(.persist)").last().prependTo($hlinks);
-
-      availableSpace = $btn.hasClass("hidden") ? $nav.width() : $nav.width() - $btn.width() - 30;
-
-      // Show the dropdown btn
-      $btn.removeClass("hidden");
-    }
-
-    // The visible list is not overflowing
-  } else {
-
-    // There is space for another item in the nav
-    while (breaks.length > 0 && availableSpace > breaks[breaks.length - 1]) {
-      // Move the item to the visible list
-      if ($vlinks_persist_tail.children().length > 0) {
-        $hlinks.children().first().insertBefore($vlinks_persist_tail);
-      } else {
-        $hlinks.children().first().appendTo($vlinks);
-      }
-      breaks.pop();
-    }
-
-    // Hide the dropdown btn if hidden list is empty
-    if (breaks.length < 1) {
-      $btn.addClass('hidden');
-      $btn.removeClass('close');
-      $hlinks.addClass('hidden');
+      $vlinks.children('*:not(.persist)').last().prependTo($hlinks);
+      availableSpace = $btn.hasClass('hidden') ? $nav.width() : $nav.width() - $btn.width() - 30;
+      $btn.removeClass('hidden');
     }
   }
 
-  // Keep counter updated
-  $btn.attr("count", breaks.length);
-
-  // update masthead height and the body/sidebar top padding
-  var mastheadHeight = $('.masthead').height();
-  $('body').css('padding-top', mastheadHeight + 'px');
-  if ($(".author__urls-wrapper button").is(":visible")) {
-    $(".sidebar").css("padding-top", "");
-  } else {
-    $(".sidebar").css("padding-top", mastheadHeight + "px");
+  if (breaks.length < 1) {
+    $btn.addClass('hidden');
+    $btn.removeClass('close');
+    $hlinks.addClass('hidden');
   }
 
+  $btn.attr('count', $hlinks.children().length);
+  updateLayoutOffsets();
 }
 
 // Window listeners
@@ -74,12 +77,12 @@ function updateNav() {
 $(window).on('resize', function () {
   updateNav();
 });
-if (screen.orientation && typeof screen.orientation.addEventListener === "function") {
-  screen.orientation.addEventListener("change", function () {
+if (screen.orientation && typeof screen.orientation.addEventListener === 'function') {
+  screen.orientation.addEventListener('change', function () {
     updateNav();
   });
 } else {
-  window.addEventListener("orientationchange", function () {
+  window.addEventListener('orientationchange', function () {
     updateNav();
   });
 }
